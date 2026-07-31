3ef1ff4b047577b90b037b42ea7f108107d8c0fc
jackychia09 <jackysu8891@gmail.com>
2026-07-31 13:33:43 +0800

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,157 @@
+document.addEventListener(
+'DOMContentLoaded',
+async function(){
+
+
+const calendarEl =
+document.getElementById('calendar');
+
+
+
+const apiURL =
+`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${GOOGLE_API_KEY}&singleEvents=true&orderBy=startTime`;
+
+
+
+const response =
+await fetch(apiURL);
+
+
+const data =
+await response.json();
+
+
+
+const events =
+data.items.map(event=>{
+
+
+return {
+
+
+title:
+event.summary || "未命名課程",
+
+
+start:
+event.start.dateTime ||
+event.start.date,
+
+
+
+backgroundColor:
+getColor(event.colorId),
+
+
+borderColor:
+getColor(event.colorId),
+
+
+textColor:"#ffffff",
+
+
+
+extendedProps:{
+
+
+description:
+event.description || "",
+
+
+location:
+event.location || ""
+
+
+}
+
+
+
+}
+
+
+});
+
+
+
+const calendar =
+new FullCalendar.Calendar(
+calendarEl,
+{
+
+
+initialView:
+'dayGridMonth',
+
+
+locale:
+'zh-tw',
+
+
+height:
+'auto',
+
+
+events:events,
+
+
+
+eventClick:function(info){
+
+
+alert(
+
+info.event.title
++
+"\n\n"
++
+"地點："
++
+(info.event.extendedProps.location || "無")
++
+"\n\n"
++
+(info.event.extendedProps.description || "")
+
+);
+
+
+}
+
+
+});
+
+
+
+calendar.render();
+
+
+});
+
+
+
+
+
+function getColor(colorId){
+
+
+const colors={
+
+"1":"#7986cb",
+"2":"#33b679",
+"3":"#8e24aa",
+"4":"#e67c73",
+"5":"#f6bf26",
+"6":"#f4511e",
+"7":"#039be5",
+"8":"#616161",
+"9":"#3f51b5",
+"10":"#0b8043",
+"11":"#d60000"
+
+};
+
+
+return colors[colorId] || "#3788d8";
+
+
+}
